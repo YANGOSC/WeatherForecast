9082c7d7f0f2599c248c13ac641900fa085b90bf
Oscar <liu.yang9@hpe.com>
2016-02-12 02:11:41 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/com/weatherforecast/app/model/Province.java
@@ -0,0 +1,36 @@
+package com.weatherforecast.app.model;
+
+/**
+ * Created by yanliu on 2016/1/12.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
